3b91c8a57854ae130e0312e86b4a3f2913a083c9
Sebstian Golebiowski <kontakt@sebastian-webdev.pl>
2019-04-02 15:31:43 +0200

    mod CORS HEaders

--- src/index.js
+++ src/index.js
@@ -18,6 +18,8 @@ const port = process.env.PORT || 3000;
 app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', ['*']);
     res.append('Access-Control-Allow-Credentials', true);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 app.use(express.json()) // express will automaticly parse data to json
