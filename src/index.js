7c6fce0b06d054bd08c01713ef32f0ce7a6dfbb2
Sebstian Golebiowski <kontakt@sebastian-webdev.pl>
2019-04-03 12:43:54 +0200

    add rules for header (CORS policy)

--- src/index.js
+++ src/index.js
@@ -19,7 +19,8 @@ app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', ['*']);
     res.append('Access-Control-Allow-Credentials', true);
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    res.append('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
     next();
 });
 app.use(express.json()) // express will automaticly parse data to json
